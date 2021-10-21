355d5705bf97b8b88900d3e9451b63b976d36907
Barry <937454126@qq.com>
2021-10-22 00:27:10 +0800

    4-6 网站首页布局开发（出现小错误！不知道原因！）

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home">
-    <a-button>Default Button</a-button>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
