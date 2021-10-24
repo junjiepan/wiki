73c5c564e5828c8915a60702d3428448163da5a5
Barry <937454126@qq.com>
2021-10-24 14:28:57 +0800

    6-6 首页显示电子书列表，使用固定的分页参数，一次查出所有的电子书

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -76,29 +76,20 @@
 import { defineComponent, onMounted, ref} from 'vue';
 import axios from 'axios';
 
-const listData: Record<string, string>[] = [];
-
-for (let i = 0; i < 23; i++) {
-  listData.push({
-    href: 'https://www.antdv.com/',
-    title: `ant design vue part ${i}`,
-    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-    description:
-        'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-    content:
-        'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-  });
-}
-
 export default defineComponent({
   name: 'home',
   setup(){
     const ebooks = ref();
 
     onMounted(() => {
-      axios.get("/ebook/list").then((response) => {
+      axios.get("/ebook/list",{
+        params: {
+          page: 1,
+          size: 1000
+        }
+      }).then((response) => {
         const data = response.data;
-        ebooks.value = data.content;
+        ebooks.value = data.content.list;
       });
     });
 
@@ -116,7 +107,6 @@ export default defineComponent({
 
     return {
       ebooks,
-      listData,
       pagination,
       actions,
     }
