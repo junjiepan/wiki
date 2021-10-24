5f01546a6424faba6f5bb11625a8e92d420cc7e3
Barry <937454126@qq.com>
2021-10-24 15:00:07 +0800

    6-7 点击编辑按钮时，弹出状态框

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -19,7 +19,7 @@
 
         <template v-slot:action="{ text,record }">
           <a-space>
-            <a-button type="primary" >编辑</a-button>
+            <a-button type="primary" @click="edit">编辑</a-button>
             <a-button danger >删除</a-button>
           </a-space>
         </template>
@@ -29,6 +29,16 @@
     </a-layout-content>
 
   </a-layout>
+
+  <a-modal
+      title="电子书表单"
+      v-model:visible="modalVisible"
+      :confirm-loading="modalLoading"
+      @ok="handleModalOk"
+  >
+    <p>text</p>
+  </a-modal>
+
 </template>
 
 <script lang="ts">
@@ -117,6 +127,25 @@ export default defineComponent({
       });
     };
 
+    // -------- 表单 ---------
+    const modalVisible = ref(false);
+    const modalLoading = ref(false);
+    const handleModalOk = () => {
+      modalLoading.value = true;
+      setTimeout(() =>{
+        modalVisible.value = false;
+        modalLoading.value = false;
+      },2000)
+    };
+
+    /**
+     * 编辑
+     */
+    const edit = () => {
+      modalVisible.value = true;
+    };
+
+
     onMounted(() => {
       handleQuery({
         page: 1,
@@ -129,7 +158,12 @@ export default defineComponent({
       pagination,
       columns,
       loading,
-      handleTableChange
+      handleTableChange,
+      edit,
+
+      modalVisible,
+      modalLoading,
+      handleModalOk
     }
   }
 });
