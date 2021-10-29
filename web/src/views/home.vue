4f33413d0398384fdda907d7810e03f5cca181a0
Barry <937454126@qq.com>
2021-10-29 10:43:45 +0800

    7-8 默认显示欢迎页面，点击欢迎页面菜单时，显示欢迎页面，点击分类菜单时，显示电子书，只有二级分类才可以触发click事件

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -8,10 +8,8 @@
           @click="handleClick"
       >
         <a-menu-item key="welcome">
-          <router-link :to="'/'">
             <MailOutlined />
             <span>欢迎</span>
-          </router-link>
         </a-menu-item>
         <a-sub-menu v-for="item in level1" :key="item.id" >
           <template v-slot:title>
@@ -30,7 +28,11 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      <a-list item-layout="vertical" size="large" :grid="{ gutter: 20, column: 3}"  :data-source="ebooks">
+      <div class = "welcome" v-show = "isShowWelcome">
+        <h1>欢迎使用wiki知识库</h1>
+      </div>
+
+      <a-list v-show = "!isShowWelcome" item-layout="vertical" size="large" :grid="{ gutter: 20, column: 3}"  :data-source="ebooks">
         <template #renderItem="{ item }">
           <a-list-item key="item.name">
             <template #actions>
@@ -86,9 +88,18 @@ export default defineComponent({
       });
     };
 
-    const handleClick = () => {
-      console.log("menu click")
+    const isShowWelcome = ref(true);
+
+    const handleClick = (value: any) => {
+      console.log("menu click",value)
+      // if (value.key === 'welcome'){
+      //   isShowWelcome.value = true;
+      // }else{
+      //   isShowWelcome.value = false;
+      // }
+      isShowWelcome.value = value.key === 'welcome';
     }
+
 
     onMounted(() => {
       handleQueryCategory();
@@ -120,7 +131,9 @@ export default defineComponent({
       pagination,
       actions,
       handleClick,
-      level1
+      level1,
+
+      isShowWelcome
     }
 
   }
