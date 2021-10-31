81bd58b71bde7d695cde509c0e662188dbace0f0
Barry <937454126@qq.com>
2021-10-31 13:34:39 +0800

    9-6 编辑的时候，不修改密码

--- src/main/java/com/jiawa/wiki/service/UserService.java
+++ src/main/java/com/jiawa/wiki/service/UserService.java
@@ -87,7 +87,7 @@ public class UserService {
         } else {
             // 更新
             user.setLoginName(null);
-            user.setPassword(null);
+             user.setPassword(null);
             userMapper.updateByPrimaryKeySelective(user);
         }
     }
