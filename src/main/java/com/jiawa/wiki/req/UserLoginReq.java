3f596fd465b15a1a591201b088c34c54d357c25e
Barry <937454126@qq.com>
2021-10-31 14:22:17 +0800

    9-8 前后端调试成功，完成基本的登录功能

--- src/main/java/com/jiawa/wiki/req/UserLoginReq.java
+++ src/main/java/com/jiawa/wiki/req/UserLoginReq.java
@@ -5,10 +5,10 @@ import javax.validation.constraints.Pattern;
 
 public class UserLoginReq {
 
-    @NotEmpty(message = "【用户名】不能为空")
+    @NotEmpty(message = "【用户名】不能为空" )
     private String loginName;
 
-    @NotEmpty(message = "【密码】不能为空")
+    @NotEmpty(message = "【密码】不能为空" )
     // @Length(min = 6, max = 20, message = "【密码】6~20位")
     @Pattern(regexp = "^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,32}$", message = "【密码】规则不正确")
     private String password;
