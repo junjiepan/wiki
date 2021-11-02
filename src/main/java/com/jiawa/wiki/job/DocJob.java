a1f0d7d267d9bfe5cdd5fcfe10931963fdbd7b54
Barry <937454126@qq.com>
2021-11-02 13:17:21 +0800

    10-7 增加日志流水号，方便生产运维

--- src/main/java/com/jiawa/wiki/job/DocJob.java
+++ src/main/java/com/jiawa/wiki/job/DocJob.java
@@ -1,12 +1,14 @@
  package com.jiawa.wiki.job;
 
  import com.jiawa.wiki.service.DocService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
+ import com.jiawa.wiki.util.SnowFlake;
+ import org.slf4j.Logger;
+ import org.slf4j.LoggerFactory;
+ import org.slf4j.MDC;
+ import org.springframework.scheduling.annotation.Scheduled;
+ import org.springframework.stereotype.Component;
 
-import javax.annotation.Resource;
+ import javax.annotation.Resource;
 
  @Component
  public class DocJob {
@@ -16,11 +18,16 @@ import javax.annotation.Resource;
      @Resource
      private DocService docService;
 
+     @Resource
+     private SnowFlake snowFlake;
+
      /**
       * 每30秒更新电子书信息
       */
-    @Scheduled(cron = "1/5 * * * * ?")
+    @Scheduled(cron = "5/30 * * * * ?")
     public void cron() {
+        // 增加日志流水号
+        MDC.put("LOG_ID", String.valueOf(snowFlake.nextId()));
         LOG.info("更新电子书下的文档数据开始");
         long start = System.currentTimeMillis();
         docService.updateEbookInfo();
