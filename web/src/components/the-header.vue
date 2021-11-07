582f7df5bd7b73fb224d70fddc5fe8647f181b02
Barry <937454126@qq.com>
2021-11-08 00:19:56 +0800

    11-9 修改 Logo 展示

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -1,6 +1,6 @@
 <template>
   <a-layout-header class="header">
-    <div class="logo" />
+    <div class="logo" > wiki 知识库</div>
 
     <a class="login-menu" v-show="!user.id" @click="showLoginModal">
       <span>登录</span>
@@ -141,6 +141,15 @@ export default defineComponent({
 </script>
 
 <style>
+.logo {
+  width: 120px;
+  height: 31px;
+  /*background: rgba(255, 255, 255, 0.2);*/
+  /*margin: 16px 28px 16px 0;*/
+  float: left;
+  color: white;
+  font-size: 18px;
+}
 
 .login-menu {
   float: right;
